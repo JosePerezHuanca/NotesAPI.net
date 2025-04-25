10c885c9c0d895b0afe1dcf1a9bb56939ad0b65b
Jose Perez Huanca <perezhuancajose@gmail.com>
2025-04-25 16:01:40 -0300

    DataAnnotations

--- NotesAPI/Models/Note.cs
+++ NotesAPI/Models/Note.cs
@@ -1,10 +1,16 @@
-﻿namespace NotesAPI.Models
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace NotesAPI.Models
 {
     public class Note
     {
         public int Id { get; set; }
+        ['Required']
         public string Title { get; set; }
+        ['Required']
         public string Content { get; set; }
+        ['Required']
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
     }
