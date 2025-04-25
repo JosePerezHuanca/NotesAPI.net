3a5d4b0380d538a22fd36f0fa511b0a2fedad3ad
Jose Perez Huanca <perezhuancajose@gmail.com>
2025-04-25 16:19:19 -0300

    Contexto

---
+++ NotesAPI/Data/NoteContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NotesAPI.Models;
+
+namespace NotesAPI.Data
+{
+    public class NoteContext : DbContext
+    {
+        public NoteContext(DbContextOptions<NoteContext> options) : base(options) { }
+        public DbSet<Note> Notes {  get; set; }
+    }
+}
