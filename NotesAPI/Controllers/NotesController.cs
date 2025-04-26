2e41283bb4e5c4154a2f5304c1f75ecd7000abfb
Jose Perez Huanca <perezhuancajose@gmail.com>
2025-04-26 17:16:01 -0300

    NotesController

---
+++ NotesAPI/Controllers/NotesController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NotesAPI.Data;
+using NotesAPI.Models;
+
+namespace NotesAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class NotesController : ControllerBase
+    {
+        private readonly NoteContext _context;
+        public NotesController(NoteContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetNotes()
+        {
+            var notes = await _context.Notes.ToListAsync();
+            return Ok(notes);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetNote(int id)
+        {
+            var note = await _context.Notes.FindAsync(id);
+            if (note == null)
+            {
+                return NotFound();
+            }
+            return Ok(note);
+        }
+    }
+}
