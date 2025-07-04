21ccc037d6caed1f7433b3dd2ee80a59cce6af25
Jose Perez Huanca <perezhuancajose@gmail.com>
2025-07-04 14:54:28 -0300

    Clase genérica ApiResponse

---
+++ NotesAPI/Response/ApiResponse.cs
@@ -0,0 +1,22 @@
+﻿namespace NotesAPI.Response
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public int Status { get; set; }
+        public string? Message { get; set; }
+        public string? Token { get; set; }
+        public T? Data { get; set; }
+        public Dictionary<string, List<string>>? Errors { get; set; }
+
+        public ApiResponse(bool success, int status, string? message = null, string? token = null, T? data = default, Dictionary<string, List<string>>? errors = null)
+        {
+            Success = success;
+            Status = status;
+            Message = message;
+            Token = token;
+            Data = data;
+            Errors = errors;
+        }
+    }
+}
